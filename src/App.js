f8d7838111623f7720fd66b9631346372dec6d7f
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-30 00:44:34 -0600

    router added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <p>hi</p>
           {this.props.children}
       </div>
     );
--- src/route.js
+++ src/route.js
@@ -1,6 +1,7 @@
 import React from  'react';
 import { IndexRoute,Router, Route, hashHistory } from 'react-router';
-
+import App from './App';
+import Home from './components/Home/Home';
 export default (
     <Router history="{hashHistory}">
          <Route path="/" components={App}>
