59760400752ffd6bffe7098c3029cc93bc20887c
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-31 13:01:25 -0600

    removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+//compponents
+
+import Header from './components/header/header';
 
 class App extends Component {
   render() {
@@ -11,9 +14,27 @@ class App extends Component {
               <button type="button" className="btn btn-default">Right</button>
           </div>
            <p className="hak">Krishna</p>
+          <Header />
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+base structure of component
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
+}
+
+export default App;*/
