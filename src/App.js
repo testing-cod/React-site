626ffcf97044fb0e2efc59cf4de6d6990dabf9fa
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-31 12:24:38 -0600

    styles added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>hi</p>
-
+          <div className="btn-group" role="group" aria-label="...">
+              <button type="button" className="btn btn-default">Left</button>
+              <button type="button" className="btn btn-default">Middle</button>
+              <button type="button" className="btn btn-default">Right</button>
+          </div>
+           <p className="hak">Krishna</p>
       </div>
     );
   }
