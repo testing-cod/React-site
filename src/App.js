1c4e0434de36e446591606f1b459d9f1652c5149
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-31 17:37:38 -0600

    components added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Header from './components/header/Header';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <div className="btn-group" role="group" aria-label="...">
-              <button type="button" className="btn btn-default">Left</button>
-              <button type="button" className="btn btn-default">Middle</button>
-              <button type="button" className="btn btn-default">Right</button>
-          </div> 
+      <div className="App"> 
           <Header /> 
       </div>
     );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+//components
 
 
+// template
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-          <p>this is header </p> 
-      </div>
+      <header>
+          <div className="logo">
+             logo
+          </div>
+          <nav>
+              <ul>
+                  <li>
+                      <a href="#">Home </a>
+                  </li>
+                  <li>
+                      <a href="#">About </a>
+                  </li>
+                  <li>
+                      <a href="#">Projects </a>
+                  </li>
+                  <li>
+                      <a href="#">ContactUs</a>
+                  </li>
+              </ul>
+          </nav>
+      </header>
     );
   }
 }
