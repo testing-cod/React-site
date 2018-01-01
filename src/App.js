943741af6daf9b1b3be0c2247103463356165a09
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-31 18:16:41 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header';
+import About from './components/About/about';
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -11,6 +13,7 @@ class App extends Component {
               <button type="button" className="btn btn-default">Right</button>
           </div> 
           <Header /> 
+          <About />
       </div>
     );
   }
