6020309b68a59818baffa59a0d99d25a05fe0cc6
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-31 21:13:49 -0600

    navbar added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,77 +1,63 @@
 import React, { Component } from 'react';
-
+import './header.css';
 class Header extends Component {
   render() {
     return (
       <nav class="navbar is-transparent">
         <div class="navbar-brand">
           <a class="navbar-item" href="/">
-              CYBERDUDE
+              <img src="https://s18.postimg.org/gtu2ychmx/anonymous_security_starter_handbook_by_m4xm0u53-.png" alt="cyber" class="logo"/>
           </a>
-          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
+          <div class="navbar-start">
+              <a class="navbar-item" href="/">Home</a>
+          </div>
+          <div class="navbar-start">
+              <a class="navbar-item" href="/">Projects</a>
+          </div>
+          <div class="navbar-start">
+              <a class="navbar-item" href="/">Site-Map</a>
+          </div>
+          <div class="navbar-start">
+              <a class="navbar-item" href="/">AboutUs</a>
           </div>
         </div>
-
         <div id="navbarExampleTransparentExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item" href="https://bulma.io/">
-              Home
-            </a>
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link" href="/documentation/overview/start/">
-                Docs
-              </a>
-              <div class="navbar-dropdown is-boxed">
-                <a class="navbar-item" href="/documentation/overview/start/">
-                  Overview
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
-                  Modifiers
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                  Columns
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                  Layout
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                  Form
-                </a>
-                <hr class="navbar-divider"/>
-                <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                  Elements
-                </a>
-                <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                  Components
-                </a>
-              </div>
-            </div>
-          </div>
-
           <div class="navbar-end">
             <div class="navbar-item">
               <div class="field is-grouped">
-                <p class="control">
-                  <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
-                    <span class="icon">
-                      <i class="fa fa-twitter"></i>
-                    </span>
-                    <span>
-                      Tweet
-                    </span>
-                  </a>
-                </p>
-                <p class="control">
-                  <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.5.1.zip">
-                    <span class="icon">
-                      <i class="fa fa-download"></i>
-                    </span>
-                    <span>Download</span>
-                  </a>
-                </p>
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link" href="/documentation/overview/start/">
+                      More
+                    </a>
+                    <div class="navbar-dropdown is-boxed">
+                      <a class="navbar-item" href="/">
+                        Clients
+                      </a>
+                      <a class="navbar-item" href="/">
+                        Services
+                      </a>
+                      <a class="navbar-item" href="/">
+                        OurTeam
+                      </a>
+                      <hr class="navbar-divider"/>
+                      <a class="navbar-item" href="/">
+                        Contact
+                      </a>
+                      <a class="navbar-item" href="/">
+                        Components
+                      </a>
+                    </div>
+                 </div>
+                  <p class="control">
+                    <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" href="/">
+                      <span class="icon">
+                        <i class="fa fa-twitter"></i>
+                      </span>
+                      <span>
+                      Login
+                      </span>
+                    </a>
+                  </p>
               </div>
             </div>
           </div>
