ece73f170d0e4749a0acfb1891ef665aaa177dea
krishnaUIDev <krishna.kondoju007@gmail.com>
2018-01-01 01:25:20 -0600

    files added

---
+++ src/components/Clients/clients.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Clients extends Component {
+    render() {
+        return (
+            <div>
+                <p>clients</p>
+            </div>
+        );
+    }
+}
+
+export default Clients;
---
+++ src/components/sitemap/sitemap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sitemap extends Component {
+    render() {
+        return (
+            <div>
+             <p>this is sitemap</p>   
+            </div>
+        );
+    }
+}
+
+export default Sitemap;
