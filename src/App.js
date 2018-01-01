514c1405ece0550f2c5cfa220bd53b3d65ea27c0
krishnaUIDev <krishna.kondoju007@gmail.com>
2018-01-01 01:48:00 -0600

    routes upadted

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Header from './components/header/header';
 // import Footer from './components/footer/footer';
 import Home from './components/Home/home';
 import About from './components/About/about';
+import Clients from './components/Clients/clients';
+import Contact from './components/Contact/contact';
+import Projects from './components/Projects/projects';
+import Services from './components/Services/services';
 
 class App extends Component {
   render() {
@@ -14,8 +18,13 @@ class App extends Component {
       <Router>
         <div className="App"> 
             <Header />
+            {/* Router */}
                 <Route exact path='/' component={Home}/>
                 <Route exact path='/about' component={About}/> 
+                <Route exact path='/Clients' component={Clients}/>
+                <Route exact path='/Contact' component={Contact}/>
+                <Route exact path='/Projects' component={Projects}/>
+                <Route exact path='/Services' component={Services}/>
         </div>
       </Router>
     );
