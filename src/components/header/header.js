357e0719f3859096d8a2e75d4b5f9527d4391376
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-12-31 21:31:54 -0600

    navbar added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,55 +3,55 @@ import './header.css';
 class Header extends Component {
   render() {
     return (
-      <nav class="navbar is-transparent">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-              <img src="https://s18.postimg.org/gtu2ychmx/anonymous_security_starter_handbook_by_m4xm0u53-.png" alt="cyber" class="logo"/>
+      <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+              <img src="https://s18.postimg.org/gtu2ychmx/anonymous_security_starter_handbook_by_m4xm0u53-.png" alt="cyber" className="logo"/>
           </a>
-          <div class="navbar-start">
-              <a class="navbar-item" href="/">Home</a>
+          <div className="navbar-start">
+              <a className="navbar-item" href="/"><span className="icon"><i className="fa fa-home" aria-hidden="true"></i></span> Home </a>
           </div>
-          <div class="navbar-start">
-              <a class="navbar-item" href="/">Projects</a>
+          <div className="navbar-start">
+              <a className="navbar-item" href="/"> <span className="icon"><i className="fa fa-pencil-square-o " aria-hidden="true"></i></span>Projects</a>
           </div>
-          <div class="navbar-start">
-              <a class="navbar-item" href="/">Site-Map</a>
+          <div className="navbar-start">
+              <a className="navbar-item" href="/"><span className="icon"><i className="fa fa-sitemap" aria-hidden="true"></i></span>Site-Map</a>
           </div>
-          <div class="navbar-start">
-              <a class="navbar-item" href="/">AboutUs</a>
+          <div className="navbar-start">
+              <a className="navbar-item" href="/">AboutUs</a>
           </div>
         </div>
-        <div id="navbarExampleTransparentExample" class="navbar-menu">
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="field is-grouped">
-                  <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link" href="/documentation/overview/start/">
+        <div id="navbarExampleTransparentExample" className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                  <div className="navbar-item has-dropdown is-hoverable">
+                    <a className="navbar-link" href="/documentation/overview/start/">
                       More
                     </a>
-                    <div class="navbar-dropdown is-boxed">
-                      <a class="navbar-item" href="/">
+                    <div className="navbar-dropdown is-boxed">
+                      <a className="navbar-item" href="/">
                         Clients
                       </a>
-                      <a class="navbar-item" href="/">
+                      <a className="navbar-item" href="/">
                         Services
                       </a>
-                      <a class="navbar-item" href="/">
+                      <a className="navbar-item" href="/">
                         OurTeam
                       </a>
-                      <hr class="navbar-divider"/>
-                      <a class="navbar-item" href="/">
+                      <hr className="navbar-divider"/>
+                      <a className="navbar-item" href="/">
                         Contact
                       </a>
-                      <a class="navbar-item" href="/">
+                      <a className="navbar-item" href="/">
                         Components
                       </a>
                     </div>
                  </div>
-                  <p class="control">
-                    <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" href="/">
-                      <span class="icon">
-                        <i class="fa fa-twitter"></i>
+                  <p className="control">
+                    <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" href="/">
+                      <span className="icon">
+                        <i className="fa fa-lock"></i>
                       </span>
                       <span>
                       Login
@@ -62,7 +62,7 @@ class Header extends Component {
             </div>
           </div>
         </div>
-    </nav>
+     </nav>
     );
   }
 }
